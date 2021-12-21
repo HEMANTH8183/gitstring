acb0c90626f877810c611a896093ee1187c864d9
HEMANTH8183 <hemanth8183.xworkz@gmail.com>
2021-12-21 08:11:50 +0530

    string file

---
+++ Hjk.java
@@ -0,0 +1,11 @@
+class Hjk {
+	static String []party = {"bringquaterwhilecomingtositting"};
+		public static void main(String []args){
+		System.out.println(party[0]);
+
+
+ byte []yestjana = {1, 2, 3, 4,};
+ System.out.println(yestjana[0]);
+		
+	}
+}
